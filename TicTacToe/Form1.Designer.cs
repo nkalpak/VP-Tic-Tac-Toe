32e08d9fe190e5dd2988935b04f782e2211d968e
nkalpak <nikitakalpakovski1@outlook.com>
2020-05-12 21:18:00 +0200

    Added clickable alternating (X/O) buttons.

---
+++ TicTacToe/Form1.Designer.cs
@@ -0,0 +1,155 @@
+﻿namespace TicTacToe
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.GameButton1 = new System.Windows.Forms.Button();
+            this.GameButton2 = new System.Windows.Forms.Button();
+            this.GameButton3 = new System.Windows.Forms.Button();
+            this.GameButton4 = new System.Windows.Forms.Button();
+            this.GameButton5 = new System.Windows.Forms.Button();
+            this.GameButton6 = new System.Windows.Forms.Button();
+            this.GameButton7 = new System.Windows.Forms.Button();
+            this.GameButton8 = new System.Windows.Forms.Button();
+            this.GameButton9 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // GameButton1
+            // 
+            this.GameButton1.Location = new System.Drawing.Point(174, 74);
+            this.GameButton1.Name = "GameButton1";
+            this.GameButton1.Size = new System.Drawing.Size(75, 75);
+            this.GameButton1.TabIndex = 0;
+            this.GameButton1.UseVisualStyleBackColor = true;
+            this.GameButton1.Click += new System.EventHandler(this.GameButton1_Click);
+            // 
+            // GameButton2
+            // 
+            this.GameButton2.Location = new System.Drawing.Point(255, 74);
+            this.GameButton2.Name = "GameButton2";
+            this.GameButton2.Size = new System.Drawing.Size(75, 75);
+            this.GameButton2.TabIndex = 0;
+            this.GameButton2.UseVisualStyleBackColor = true;
+            this.GameButton2.Click += new System.EventHandler(this.GameButton2_Click);
+            // 
+            // GameButton3
+            // 
+            this.GameButton3.Location = new System.Drawing.Point(336, 74);
+            this.GameButton3.Name = "GameButton3";
+            this.GameButton3.Size = new System.Drawing.Size(75, 75);
+            this.GameButton3.TabIndex = 0;
+            this.GameButton3.UseVisualStyleBackColor = true;
+            this.GameButton3.Click += new System.EventHandler(this.GameButton3_Click);
+            // 
+            // GameButton4
+            // 
+            this.GameButton4.Location = new System.Drawing.Point(174, 155);
+            this.GameButton4.Name = "GameButton4";
+            this.GameButton4.Size = new System.Drawing.Size(75, 75);
+            this.GameButton4.TabIndex = 0;
+            this.GameButton4.UseVisualStyleBackColor = true;
+            this.GameButton4.Click += new System.EventHandler(this.GameButton4_Click);
+            // 
+            // GameButton5
+            // 
+            this.GameButton5.Location = new System.Drawing.Point(255, 155);
+            this.GameButton5.Name = "GameButton5";
+            this.GameButton5.Size = new System.Drawing.Size(75, 75);
+            this.GameButton5.TabIndex = 0;
+            this.GameButton5.UseVisualStyleBackColor = true;
+            this.GameButton5.Click += new System.EventHandler(this.GameButton5_Click);
+            // 
+            // GameButton6
+            // 
+            this.GameButton6.Location = new System.Drawing.Point(336, 155);
+            this.GameButton6.Name = "GameButton6";
+            this.GameButton6.Size = new System.Drawing.Size(75, 75);
+            this.GameButton6.TabIndex = 0;
+            this.GameButton6.UseVisualStyleBackColor = true;
+            this.GameButton6.Click += new System.EventHandler(this.GameButton6_Click);
+            // 
+            // GameButton7
+            // 
+            this.GameButton7.Location = new System.Drawing.Point(174, 236);
+            this.GameButton7.Name = "GameButton7";
+            this.GameButton7.Size = new System.Drawing.Size(75, 75);
+            this.GameButton7.TabIndex = 0;
+            this.GameButton7.UseVisualStyleBackColor = true;
+            this.GameButton7.Click += new System.EventHandler(this.GameButton7_Click);
+            // 
+            // GameButton8
+            // 
+            this.GameButton8.Location = new System.Drawing.Point(255, 236);
+            this.GameButton8.Name = "GameButton8";
+            this.GameButton8.Size = new System.Drawing.Size(75, 75);
+            this.GameButton8.TabIndex = 0;
+            this.GameButton8.UseVisualStyleBackColor = true;
+            this.GameButton8.Click += new System.EventHandler(this.GameButton8_Click);
+            // 
+            // GameButton9
+            // 
+            this.GameButton9.Location = new System.Drawing.Point(336, 236);
+            this.GameButton9.Name = "GameButton9";
+            this.GameButton9.Size = new System.Drawing.Size(75, 75);
+            this.GameButton9.TabIndex = 0;
+            this.GameButton9.UseVisualStyleBackColor = true;
+            this.GameButton9.Click += new System.EventHandler(this.GameButton9_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.GameButton9);
+            this.Controls.Add(this.GameButton6);
+            this.Controls.Add(this.GameButton3);
+            this.Controls.Add(this.GameButton8);
+            this.Controls.Add(this.GameButton7);
+            this.Controls.Add(this.GameButton5);
+            this.Controls.Add(this.GameButton4);
+            this.Controls.Add(this.GameButton2);
+            this.Controls.Add(this.GameButton1);
+            this.Name = "Form1";
+            this.Text = "Tic Tac Toe";
+            this.ResumeLayout(false);
+        }
+
+        private System.Windows.Forms.Button GameButton1;
+        private System.Windows.Forms.Button GameButton2;
+        private System.Windows.Forms.Button GameButton3;
+        private System.Windows.Forms.Button GameButton4;
+        private System.Windows.Forms.Button GameButton5;
+        private System.Windows.Forms.Button GameButton6;
+        private System.Windows.Forms.Button GameButton7;
+        private System.Windows.Forms.Button GameButton8;
+        private System.Windows.Forms.Button GameButton9;
+
+        #endregion
+    }
+}
---
+++ TicTacToe/Form1.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace TicTacToe
+{
+    public partial class Form1 : Form
+    {
+        private const int BOARD_WIDTH = 3; 
+        
+        private string[,] _board = new string[3, 3];
+        private string _currentSign = "X";
+
+        private bool IsIndexValid(int index) => index >= 0 && index < 9;
+
+        private bool IsBoardSignedAtPosition(int index)
+        {
+            int x = index % BOARD_WIDTH;
+            int y = index / BOARD_WIDTH;
+
+            return _board[x, y] != null;
+        }
+
+        private void SignBoardAtPosition(int index, string sign)
+        {
+            if (!IsIndexValid(index)) throw new IndexOutOfRangeException("Button positions are only [0-8].");
+
+            int x = index % BOARD_WIDTH;
+            int y = index / BOARD_WIDTH;
+
+            _board[x, y] = sign;
+        }
+
+        private void ToggleSign()
+        {
+            _currentSign = _currentSign == "X" ? "O" : "X";
+        }
+
+        private void HandleButtonSignRender(Button button)
+        {
+            string buttonName = button.Name;
+            string lastChar = buttonName.Substring(buttonName.Length - 1);
+            int buttonIndex = int.Parse(lastChar) - 1;
+
+            if (IsBoardSignedAtPosition(buttonIndex)) return;
+            
+            SignBoardAtPosition(buttonIndex, _currentSign);
+            button.Text = _currentSign;
+            
+            ToggleSign();
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void GameButton1_Click(object sender, EventArgs e)
+        {
+            HandleButtonSignRender(sender as Button);
+        }
+
+        private void GameButton2_Click(object sender, EventArgs e)
+        {
+            HandleButtonSignRender(sender as Button);
+        }
+
+        private void GameButton3_Click(object sender, EventArgs e)
+        {
+            HandleButtonSignRender(sender as Button);
+        }
+        
+        private void GameButton4_Click(object sender, EventArgs e)
+        {
+            HandleButtonSignRender(sender as Button);
+        }
+        
+        private void GameButton5_Click(object sender, EventArgs e)
+        {
+            HandleButtonSignRender(sender as Button);
+        }
+        
+        private void GameButton6_Click(object sender, EventArgs e)
+        {
+            HandleButtonSignRender(sender as Button);
+        }
+        
+        private void GameButton7_Click(object sender, EventArgs e)
+        {
+            HandleButtonSignRender(sender as Button);
+        }
+        
+        private void GameButton8_Click(object sender, EventArgs e)
+        {
+            HandleButtonSignRender(sender as Button);
+        }
+        
+        private void GameButton9_Click(object sender, EventArgs e)
+        {
+            HandleButtonSignRender(sender as Button);
+        }
+    }
+}
