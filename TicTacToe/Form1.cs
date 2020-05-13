66b8b46d1e52aec6629bca9d844909fe3760766f
nkalpak <nikitakalpakovski1@outlook.com>
2020-05-13 10:40:50 +0200

    Refactored board into its own class.

--- TicTacToe/Form1.cs
+++ TicTacToe/Form1.cs
@@ -7,97 +7,21 @@ namespace TicTacToe
 {
     public partial class Form1 : Form
     {
-        private const int BoardSize = 3;
-        private bool HasWon;
-
-        private string[,] _board = new string[3, 3];
-        private string _currentSign = "X";
-
-        private bool IsIndexValid(int index) => index >= 0 && index < 9;
-
-        private bool IsBoardSignedAtPosition(int index)
-        {
-            int x = index % BoardSize;
-            int y = index / BoardSize;
-
-            return _board[x, y] != null;
-        }
-
-        private bool IsBoardHorizontalWinState()
-        {
-            var win = false;
-
-            for (var i = 0; i < BoardSize; i++)
-            {
-                string sign = _board[i, 0];
-                win = true;
-
-                for (var j = 1; j < BoardSize; j++)
-                {
-                    string boardSign = _board[i, j];
-                    if (boardSign == null || sign == null || !boardSign.Equals(sign)) win = false;
-                }
-
-                if (win) return win;
-            }
-
-            return win;
-        }
+        private readonly GameBoard _gameBoard = new GameBoard();
         
-        private bool IsBoardVerticalWinState()
-        {
-            var win = false;
-
-            for (var i = 0; i < BoardSize; i++)
-            {
-                string sign = _board[0, i];
-                win = true;
-
-                for (var j = 1; j < BoardSize; j++)
-                {
-                    string boardSign = _board[j, i];
-                    if (boardSign == null || sign == null || !boardSign.Equals(sign)) win = false;
-                }
-
-                if (win) return win;
-            }
-
-            return win;
-        }
-        
-        private void SignBoardAtPosition(int index, string sign)
-        {
-            if (!IsIndexValid(index)) throw new IndexOutOfRangeException("Button positions are only [0-8].");
-
-            int x = index % BoardSize;
-            int y = index / BoardSize;
-
-            _board[x, y] = sign;
-        }
-
-        private void ToggleSign()
-        {
-            _currentSign = _currentSign == "X" ? "O" : "X";
-        }
-
         private void HandleButtonSignRender(Button button)
         {
             string buttonName = button.Name;
             string lastChar = buttonName.Substring(buttonName.Length - 1);
             int buttonIndex = int.Parse(lastChar) - 1;
 
-            if (IsBoardSignedAtPosition(buttonIndex)) return;
+            string sign = _gameBoard.SignBoardAtPosition(buttonIndex);
+            button.Text = sign;
 
-            SignBoardAtPosition(buttonIndex, _currentSign);
-            if (IsBoardHorizontalWinState() || IsBoardVerticalWinState())
+            if (_gameBoard.HasWon)
             {
-                HasWon = true;
                 WinnerDisplay.Text = "Someone has won!";
             }
-
-            button.Text = _currentSign;
-
-            ToggleSign();
         }
 
         public Form1()
---
+++ TicTacToe/GameBoard.cs
@@ -0,0 +1,100 @@
+﻿using System;
+
+namespace TicTacToe
+{
+    public class GameBoard
+    {
+        private const int BoardSize = 3;
+        public bool HasWon { get; private set; }
+
+        private readonly string[,] _board = new string[3, 3];
+        private string _currentSign = "O";
+
+        private bool IsIndexValid(int index) => index >= 0 && index < 9;
+
+        private bool IsBoardHorizontalWinState()
+        {
+            var win = false;
+
+            for (var i = 0; i < BoardSize; i++)
+            {
+                string sign = _board[i, 0];
+                win = true;
+
+                for (var j = 1; j < BoardSize; j++)
+                {
+                    string boardSign = _board[i, j];
+                    if (boardSign == null || sign == null || !boardSign.Equals(sign)) win = false;
+                }
+
+                if (win) return win;
+            }
+
+            return win;
+        }
+        
+        private bool IsBoardVerticalWinState()
+        {
+            var win = false;
+
+            for (var i = 0; i < BoardSize; i++)
+            {
+                string sign = _board[0, i];
+                win = true;
+
+                for (var j = 1; j < BoardSize; j++)
+                {
+                    string boardSign = _board[j, i];
+                    if (boardSign == null || sign == null || !boardSign.Equals(sign)) win = false;
+                }
+
+                if (win) return win;
+            }
+
+            return win;
+        }
+
+        private void DetermineIfPlayerHasWon()
+        {
+            if (IsBoardHorizontalWinState() || IsBoardVerticalWinState()) HasWon = true;
+        }
+        
+        /// <summary>
+        /// Method which signs the board with either X or O at the given index position.
+        /// </summary>
+        /// <param name="index">The 1D index position at which the 2D board should be signed.</param>
+        /// <returns>The appropriate sign which was inserted at the given position. (determined by internal logic)</returns>
+        public string SignBoardAtPosition(int index)
+        {
+            if (!IsIndexValid(index)) throw new IndexOutOfRangeException("Button positions are only [0-8].");
+            if (GetSignAtPosition(index) != null) return GetSignAtPosition(index);
+            
+            int x = index % BoardSize;
+            int y = index / BoardSize;
+
+            SetSignOn2dBoard(x, y);
+            DetermineIfPlayerHasWon();
+
+            return _currentSign;
+        }
+
+        private void SetSignOn2dBoard(int x, int y)
+        {
+            ToggleSign();
+            _board[x, y] = _currentSign;
+        }
+        
+        private string GetSignAtPosition(int index)
+        {
+            int x = index % BoardSize;
+            int y = index / BoardSize;
+
+            return _board[x, y];
+        }
+
+        private void ToggleSign()
+        {
+            _currentSign = _currentSign == "X" ? "O" : "X";
+        }
+    }
+}
